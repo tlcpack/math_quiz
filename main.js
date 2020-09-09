cb86bff2b913d10e68d171794436b07892115762
Taylor Cooke <taylorcooke@gmail.com>
2020-09-08 20:51:09 -0400

    comparing sum and input

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 function query (selector) {
-    return document.querySelector(selector)
+  return document.querySelector(selector)
 }
 
 const buttonBlock = query('.button')
 let questionBlock = query('.question')
-// let answerBlock = query('.answer')
+let answerBlock = query('.answer')
 
-function createQuestion () {
-    const n = 2
-    const m = 3
-    questionBlock.innerHTML = `${n} + ${m} = `
+const n = 2
+const m = 3
+const sum = n + m
+
+function createQuestion (n, m) {
+  questionBlock.innerHTML = `${n} + ${m} = `
+}
+
+function compareAnswer (user) {
+  if (user == sum) {
+    console.log('yay')
+  } else {
+    console.log('nay')
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    buttonBlock.addEventListener('click', function (event) {
-    createQuestion()
-    })
+  buttonBlock.addEventListener('click', function (event) {
+    createQuestion(n, m)
+  })
+  answerBlock.addEventListener('change', function (event) {
+    compareAnswer(event.target.value)
+  })
 })
